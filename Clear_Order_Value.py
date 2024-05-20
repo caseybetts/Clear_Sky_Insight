56b180e69d27d241430fea170fbd8799a08a2b0a
Casey Betts <caseyandsusana@gmail.com>
2024-05-20 10:31:29 -0600

    Auto applies symbology to output

--- Clear_Order_Value.py
+++ Clear_Order_Value.py
@@ -29,12 +29,14 @@ def select_orders_by_rev(orders_layer, onv_layer, rev):
                                             "NOT_INVERT")
 
 # Create a strip overlay of the order layer with Grid Index Features
-def create_strip_overlay(orders_layer, out_feature_class):
+def create_strip_overlay(orders_layer, rev):
     """ Creates a .gdb layer containing a strip overlay of the given layer """
     
     # with arcpy.EnvManager(outputCoordinateSystem='PROJCS["WGS_1984_World_Mercator",GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Mercator"],PARAMETER["False_Easting",0.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",0.0],PARAMETER["Standard_Parallel_1",0.0],UNIT["Meter",1.0]]'):
-    arcpy.cartography.GridIndexFeatures(out_feature_class, orders_layer, "INTERSECTFEATURE", "NO_USEPAGEUNIT", None, "15 Kilometers", "60 Kilometers", None, 369, 853, None, "NO_LABELFROMORIGIN")
+    arcpy.cartography.GridIndexFeatures("CSI_" + rev + "_strips", orders_layer, "INTERSECTFEATURE", "NO_USEPAGEUNIT", None, "15 Kilometers", "60 Kilometers", None, 369, 853, None, "NO_LABELFROMORIGIN")
         
+    return "CSI_" + rev + "_strips"
+
 # Create point feature from layer
 def create_point_feature(orders_layer, out_feature_class):
     """ Given an order layer and output location this creates a point featuer at the center of each order """
@@ -68,8 +70,8 @@ def create_order_layers(prod, onv, rev):
     arcpy.management.CalculateField(prod_layer, "CSI_Value", "(100-(!tasking_priority!-700))**1.5", "PYTHON3", '', "LONG", "NO_ENFORCE_DOMAINS")
     
     # Create an overlay of strip sized polygons
-    create_strip_overlay(prod_layer, "CSI_" + rev + "_strips")
-    strip_layer = "CSI_" + rev + "_strips"
+    strip_layer = create_strip_overlay(prod_layer, rev)
+    arcpy.AddMessage("stirp layer: " + strip_layer)
     
     # Create a feature to polygon layer
     arcpy.management.FeatureToPolygon("CSI_" + rev + "_PROD" + ";" + "CSI_" + rev + "_strips", 
@@ -123,18 +125,19 @@ def add_layers_to_map(layer1):
     # Add the feature layer to the map
     map.addDataFromPath(layer1)
 
-    # l = map.listLayers()[0]
+    # Get the symbology from the symbology template layer
+    clear_orders = map.listLayers()[0]
+    source_layer_name = r"Clear_Orders_Symbology_Template"
 
-    # # Set the symbology for the layer
-    # sym = l.symbology
-
-    # # Set the symbology properties
-    # sym.colorizer.type == 'RasterClassifyColorizer'
-    # sym.colorizer.classificationField = 'Value'
-    # sym.colorizer.breakCount = 7
-    # sym.colorizer.colorRamp = project.listColorRamps('Cyan to Purple')[0]
-    # l.symbology = sym
-
+    for layer in map.listLayers():
+        if layer.name == source_layer_name:
+            source_layer = layer
+            break
+    else:
+        raise Exception(f"Source layer '{source_layer_name}' not found in the TOC.")
+    
+    # Apply the symbology to the target layer
+    clear_orders.symbology = source_layer.symbology
 
 # Run full series
 def run(prod, onv, weather, rev):
@@ -144,10 +147,10 @@ def run(prod, onv, weather, rev):
     arcpy.env.workspace = r"C:\Users\ca003927\OneDrive - Maxar Technologies Holdings Inc\Private Drop\Git\Clear_Sky_Insight\CSI_GeoDatabase.gdb\\"
 
     # Create order layers
-    create_order_layers(prod, onv, rev)
+    # create_order_layers(prod, onv, rev)
     sj_layer = "CSI_" + rev + "_SJ"
 
-    create_cloud_shape(onv, weather, rev)
+    # create_cloud_shape(onv, weather, rev)
     cloud_layer = "CSI_" + rev + "_clouds"
 
     # Create order layer in clear areas only
